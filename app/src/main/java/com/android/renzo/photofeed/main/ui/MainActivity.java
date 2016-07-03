562b76acecf71d87b6f18f977949baf9490a7b6e
Renzo Maldonado Aguilar <renzoma89@gmail.com>
2016-07-03 18:16:57 -0500

    Onconnected, on connection failed

--- app/src/main/java/com/android/renzo/photofeed/main/ui/MainActivity.java
+++ app/src/main/java/com/android/renzo/photofeed/main/ui/MainActivity.java
@@ -1,12 +1,18 @@
 package com.android.renzo.photofeed.main.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -19,6 +25,7 @@ import com.android.renzo.photofeed.login.ui.LoginActivity;
 import com.android.renzo.photofeed.main.MainPresenter;
 import com.android.renzo.photofeed.main.adapters.MainSectionsPagerAdapter;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
@@ -28,8 +35,8 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements MainView,
-                                                                GoogleApiClient.ConnectionCallbacks,
-                                                                GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
@@ -48,6 +55,9 @@ public class MainActivity extends AppCompatActivity implements MainView,
     private Location lastKnowLocation;
     private GoogleApiClient apiClient;
     private PhotoFeedApp app;
+    private boolean resolvingError = false;
+    private static final int PERMISSIONS_REQUEST_LOCATION = 1;
+    private static final int REQUEST_RESOLVE_ERROR = 0;
 
 
     @Override
@@ -63,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements MainView,
     }
 
     private void setupGoogleAPIClient() {
-        if(apiClient == null){
+        if (apiClient == null) {
             apiClient = new GoogleApiClient.Builder(this)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
@@ -103,7 +113,7 @@ public class MainActivity extends AppCompatActivity implements MainView,
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if(id == R.id.action_logout){
+        if (id == R.id.action_logout) {
             logout();
         }
         return super.onOptionsItemSelected(item);
@@ -114,8 +124,8 @@ public class MainActivity extends AppCompatActivity implements MainView,
         sharedPreferences.edit().clear().commit();
         Intent intent = new Intent(this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-                        | Intent.FLAG_ACTIVITY_NEW_TASK
-                        | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                | Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 
@@ -133,12 +143,76 @@ public class MainActivity extends AppCompatActivity implements MainView,
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[]{ Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSIONS_REQUEST_LOCATION);
+            }
+
+            return;
+        }
+        if(LocationServices.FusedLocationApi.getLocationAvailability(apiClient).isLocationAvailable()){
+            lastKnowLocation = LocationServices.FusedLocationApi.getLastLocation(apiClient);
+        }else{
+            Snackbar.make(viewPager, R.string.main_error_location_notavailable, Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSIONS_REQUEST_LOCATION: {
+                if(grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(LocationServices.FusedLocationApi.getLocationAvailability(apiClient).isLocationAvailable()){
+                        lastKnowLocation = LocationServices.FusedLocationApi.getLastLocation(apiClient);
+                    }else{
+                        Snackbar.make(viewPager, R.string.main_error_location_notavailable, Snackbar.LENGTH_SHORT).show();
+                    }
+
+                }
+                return;
+            }
+        }
 
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if(resolvingError){
+            return;
+        }else if(connectionResult.hasResolution()){
+            resolvingError = true;
+            try {
+                connectionResult.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            resolvingError = true;
+            GoogleApiAvailability.getInstance().getErrorDialog(this, connectionResult.getErrorCode(), REQUEST_RESOLVE_ERROR).show();
+        }
+    }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_RESOLVE_ERROR){
+            resolvingError = false;
+            if(resultCode == RESULT_OK){
+                if(!apiClient.isConnecting() && apiClient.isConnected()){
+                    apiClient.connect();
+                }
+
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        apiClient.connect();
     }
 
     @Override
@@ -153,11 +227,6 @@ public class MainActivity extends AppCompatActivity implements MainView,
 
     @Override
     public void onUploadError(String error) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
